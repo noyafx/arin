c1d832b27e623001de7cd9750e812642abf71c29
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-10 09:55:54 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,13 @@
 import { BaseInteraction, Client as RawClient, ClientOptions, Collection } from 'discord.js';
 
+export interface Event {
+  once?: boolean
+  execute: Function
+};
+
 export interface Command {
-  data: SlashCommandBuilder | ContextMenuCommandBuilder;
-  execute(interaction: BaseInteraction): Promise<any>;
+  data: SlashCommandBuilder | ContextMenuCommandBuilder
+  execute(interaction: BaseInteraction): Promise<any>
 };
 
 export class Client extends RawClient {
